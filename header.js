1cb4e8485d002dfc84b7139f97642d5d3212d969
IAdesu <46866205+IAdesu@users.noreply.github.com>
2020-02-17 02:01:38 +0900

    a

--- header.js
+++ header.js
@@ -1,3 +1,9 @@
-$(function() {
- $('#page').load('header.html');
-});
+function header(){
+    $.ajax({
+        url: "header.html", // 読み込むHTMLファイル
+        cache: false,
+        success: function(html){
+            document.write(html);
+        }
+    });
+}
